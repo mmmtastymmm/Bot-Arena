f546655f8c506e2ca4a6d1c11775a56ca29fd1be
Thomas Marks <tmsluigi@gmail.com>
2023-01-10 22:36:52 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use poker::Card;
+
+fn get_deck() -> Vec<Card>{
+    let deck = Card::generate_deck().collect();
+    deck
+}
+
+
+fn main() {
+    println!("Hello, world!");
+    let deck = get_deck();
+    println!("Have this many cards: {}", deck.len());
+}
+
+#[test]
+fn check_deck_size(){
+    let deck = get_deck();
+    assert_eq!(deck.len(), 52);
+}
