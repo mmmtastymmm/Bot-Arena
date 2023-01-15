dff61abbfcb56cfbe5756aad62b0f53cd44748db
Thomas Marks <tmsluigi@gmail.com>
2023-01-14 17:00:24 -0700

    Fix some linting

--- src/game_components.rs
+++ src/game_components.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::sync::Arc;
 
 use poker::{Card, Evaluator};
@@ -40,7 +39,7 @@ impl Player {
     }
 
     pub fn deal(&mut self, cards: [Card; 2]) {
-        self.player_state = PlayerState::Active { 0: ActiveState { hand: Hand { cards }, current_bet: Bet { bet: 0 } } };
+        self.player_state = PlayerState::Active(ActiveState { hand: Hand { cards }, current_bet: Bet { bet: 0 } });
     }
 }
 
@@ -73,59 +72,69 @@ impl Table {
     pub fn deal(&mut self) {
         let deck = Card::generate_shuffled_deck();
         let mut deck_iterator = deck.into_iter();
-        self.flop = Option::from([deck_iterator.next().unwrap().clone(),
-            deck_iterator.next().unwrap().clone(),
-            deck_iterator.next().unwrap().clone(),
+        self.flop = Option::from([*deck_iterator.next().unwrap(),
+            *deck_iterator.next().unwrap(),
+            *deck_iterator.next().unwrap(),
         ]);
 
-        self.turn = Option::from(deck_iterator.next().unwrap().clone());
-        self.river = Option::from(deck_iterator.next().unwrap().clone());
+        self.turn = Option::from(*deck_iterator.next().unwrap());
+        self.river = Option::from(*deck_iterator.next().unwrap());
 
         for player in &mut self.players {
-            let card1 = deck_iterator.next().unwrap().clone();
-            let card2 = deck_iterator.next().unwrap().clone();
+            let card1 = *deck_iterator.next().unwrap();
+            let card2 = *deck_iterator.next().unwrap();
             player.deal([card1, card2])
         }
     }
 }
 
 
-#[test]
-pub fn test_deal_correct_size() {
-    // Required for the table evaluator
-    let shared_evaluator = Arc::new(Evaluator::new());
-    const PLAYER_SIZE: usize = 23;
-    let mut table = Table::new(PLAYER_SIZE, shared_evaluator);
-    // Deal the largest table size allowed
-    table.deal();
-    // Make a set to make sure there are unique cards
-    let mut cards = HashSet::new();
-    for card in &table.flop.unwrap() {
-        cards.insert(card.to_string());
-    }
-    cards.insert(table.turn.unwrap().to_string());
-    cards.insert(table.river.unwrap().to_string());
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+    use std::sync::Arc;
 
-    for player in &table.players {
-        match player.player_state {
-            PlayerState::Folded => {}
-            PlayerState::Active(a) => {
-                for card in &a.hand.cards {
-                    cards.insert(card.to_string());
+    use poker::Evaluator;
+
+    use crate::game_components::{PlayerState, Table};
+
+    #[test]
+    pub fn test_deal_correct_size() {
+        // Required for the table evaluator
+        let shared_evaluator = Arc::new(Evaluator::new());
+        const PLAYER_SIZE: usize = 23;
+        let mut table = Table::new(PLAYER_SIZE, shared_evaluator);
+        // Deal the largest table size allowed
+        table.deal();
+        // Make a set to make sure there are unique cards
+        let mut cards = HashSet::new();
+        for card in &table.flop.unwrap() {
+            cards.insert(card.to_string());
+        }
+        cards.insert(table.turn.unwrap().to_string());
+        cards.insert(table.river.unwrap().to_string());
+
+        for player in &table.players {
+            match player.player_state {
+                PlayerState::Folded => {}
+                PlayerState::Active(a) => {
+                    for card in &a.hand.cards {
+                        cards.insert(card.to_string());
+                    }
                 }
             }
         }
+
+        // Check the card size is 2 * players + 5 for the 5 shared cards
+        assert_eq!(5 as usize + 2 * PLAYER_SIZE, cards.len());
     }
 
-    // Check the card size is 2 * players + 5 for the 5 shared cards
-    assert_eq!(5 as usize + 2 * PLAYER_SIZE, cards.len());
-}
-
-#[test]
-#[should_panic]
-pub fn test_deal_too_many_players() {
-    // Add to many players and expect a panic
-    let shared_evaluator = Arc::new(Evaluator::new());
-    let mut table = Table::new(24, shared_evaluator);
-    table.deal()
+    #[test]
+    #[should_panic]
+    pub fn test_deal_too_many_players() {
+        // Add to many players and expect a panic
+        let shared_evaluator = Arc::new(Evaluator::new());
+        let mut table = Table::new(24, shared_evaluator);
+        table.deal()
+    }
 }
