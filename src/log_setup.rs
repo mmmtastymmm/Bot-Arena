159660404df59bbc52d9deeddd766b15458ed74e
Thomas Marks <tmsluigi@gmail.com>
2023-09-23 15:22:38 -0600

    Make sure multiple calls to the logger init fail

--- src/log_setup.rs
+++ src/log_setup.rs
@@ -5,6 +5,14 @@ pub fn enable_logging() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 }
 
+#[test]
+#[should_panic]
+pub fn test_enable_logging() {
+    // Multiple inits should fail
+    enable_logging();
+    enable_logging();
+}
+
 /// Allow log macros to be captured by the test that calls this function
 #[test]
 pub fn enable_logging_in_test() {
