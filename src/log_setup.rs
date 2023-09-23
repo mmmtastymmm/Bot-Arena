43f77447eb5b9d97f97c27aee4648f9e02fb5e92
Thomas Marks <tmsluigi@gmail.com>
2023-09-23 15:18:35 -0600

    Add some better builds and log messages

--- src/log_setup.rs
+++ src/log_setup.rs
@@ -1,9 +1,12 @@
 use env_logger::Env;
 
+/// Call once to enable logging for the program
 pub fn enable_logging() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 }
 
+/// Allow log macros to be captured by the test that calls this function
+#[test]
 pub fn enable_logging_in_test() {
     let result = env_logger::builder().is_test(true).try_init();
     match result {
