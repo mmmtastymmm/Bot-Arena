b7990d7caa332374a7e3b9a7d2588c69d346f611
Thomas Marks <tmsluigi@gmail.com>
2023-09-26 00:17:46 -0600

    Random commit

--- src/server.rs
+++ src/server.rs
@@ -1,1 +1,18 @@
+extern crate websocket;
 
+use websocket::sync::Server as WebSocketServer;
+
+use crate::timer::Timer;
+
+pub struct Server {}
+
+impl Server {
+    pub fn from(server_url: String, connection_count: i32) -> Server {
+        let mut server = WebSocketServer::bind(server_url).unwrap();
+        let timer = Timer::new();
+        for i in 0..connection_count {
+            let connections = server.accept();
+        }
+        Server {}
+    }
+}
