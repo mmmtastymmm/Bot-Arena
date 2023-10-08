80aed6897423f5d13888c4a2ddfc646647428dfd
Thomas Marks <tmsluigi@gmail.com>
2023-10-08 00:51:39 -0600

    Add the logic for making an engine

--- src/engine.rs
+++ src/engine.rs
@@ -8,9 +8,6 @@ pub struct Engine {
 }
 
 impl Engine {
-    // All ips and let the
-    const SERVER_ADDRESS: &'static str = "0.0.0.0:0";
-
     pub async fn new(server: Server) -> Result<Engine, String> {
         if server.connections.is_empty() {
             return Err("No connections established.".to_string());
@@ -27,6 +24,7 @@ impl Engine {
 
 #[cfg(test)]
 mod tests {
+    use tokio::net::TcpStream;
     use tokio::time::Duration;
 
     use crate::server::Server;
@@ -56,11 +54,19 @@ mod tests {
         let tcp_connection = Server::get_random_tcp_listener().await;
         let address_string = format!("{}", tcp_connection.local_addr().unwrap());
 
-        // Make an engine, but make sure no one ever connects.
-        let result =
-            Engine::new(Server::from_tcp_listener(tcp_connection, server_wait_duration).await)
-                .await;
+        // Start the engine in the background
+        let server_handle = tokio::spawn(async move {
+            Engine::new(Server::from_tcp_listener(tcp_connection, server_wait_duration).await).await
+        });
+        let number_of_connections = 3;
+        for i in 0..number_of_connections {
+            info!("Trying to connect on iteration {i}");
+            let stream = TcpStream::connect(address_string.as_str()).await;
+            assert!(stream.is_ok());
+        }
+        // Check to make sure the server was constructed correctly
+        let server = server_handle.await.unwrap();
         // This should be an error as no one connected
-        assert!(result.is_err());
+        assert!(server.is_ok());
     }
 }
