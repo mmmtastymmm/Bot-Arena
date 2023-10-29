b02d070439db389989ddb4c128b081743212b9f8
Thomas Marks <tmsluigi@gmail.com>
2023-10-28 19:03:44 -0600

    Test args

--- src/args.rs
+++ src/args.rs
@@ -10,3 +10,29 @@ pub struct BotArgs {
     #[arg(short, long, default_value_t = 30.0)]
     pub server_connection_time_seconds: f64,
 }
+
+#[cfg(test)]
+mod tests {
+    use clap::Parser;
+
+    use crate::args::BotArgs;
+
+    #[test]
+    fn test_defaults() {
+        let args = BotArgs::parse_from(vec!["test"]);
+        assert_eq!(args.port, 10100);
+        assert_eq!(args.server_connection_time_seconds, 30.0);
+    }
+
+    #[test]
+    fn test_custom_port() {
+        let args = BotArgs::parse_from(vec!["test", "--port", "8080"]);
+        assert_eq!(args.port, 8080);
+    }
+
+    #[test]
+    fn test_custom_connection_time() {
+        let args = BotArgs::parse_from(vec!["test", "--server-connection-time-seconds", "45.0"]);
+        assert_eq!(args.server_connection_time_seconds, 45.0);
+    }
+}
